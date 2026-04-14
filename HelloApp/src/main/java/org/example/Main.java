4db936d9c1032e650d0812fa2bac8ddf8b717597
erlordson-gif <er.lordson@gmail.com>
2026-04-14 20:48:18 +0530

    Coded UC6: Display Hello with multiple args using substring

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -4,12 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            String names = String.join(", ", args);
-            System.out.println("Hello, " + names + "!");
-        } else {
+        if (args.length == 0) {
             System.out.println("Hello, World!");
-        }
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
 
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+
+            // Remove last ", "
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
-}
+}}
